d46e2f05b81b64e50d3a946dd07740732a875edc
alienworld1 <astronomyguy0030@gmail.com>
2024-01-17 23:38:14 +0530

    Add a header to initial page

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import "./style.css";
+
+const NavSetter = (function(){
+    function createNavigationLink(textContent) {
+        const link = document.createElement("li");
+        link.textContent = textContent;
+
+        return link;
+    }
+
+    function createNavigationBar(linkArray) {
+        const nav = document.createElement("ul");
+        nav.classList.add("nav");
+
+        linkArray.forEach(link => {
+            const linkElement = createNavigationLink(link);
+            nav.appendChild(linkElement);
+        });
+
+        return nav;
+    }
+
+    return {
+        createNavigationBar,
+    }
+})();
+
+function header() {
+    const header = document.createElement("header");
+    const logo = document.createElement("logo");
+
+    logo.textContent = "Raindrop Cafe";
+    logo.classList.add("logo");
+
+    header.appendChild(logo);
+
+    const navBar = NavSetter.createNavigationBar([
+        "Home",
+        "Menu",
+        "Contact",
+    ]);
+
+    header.appendChild(navBar);
+
+    return header;
+}
+
+//TODO: Credit the background image
+
+const content = document.querySelector("#content");
+content.appendChild(header());
