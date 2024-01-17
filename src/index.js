b833d401010d63d0b6ceb22aaf009a0b4ccc096a
alienworld1 <astronomyguy0030@gmail.com>
2024-01-18 00:04:42 +0530

    Add description of the cafe

--- src/index.js
+++ src/index.js
@@ -45,7 +45,49 @@ function header() {
     return header;
 }
 
+const MainPage = (function(){
+    function mainBody() {
+        const mainBodyElement = document.createElement("div");
+        mainBodyElement.classList.add("main-body");
+
+        return mainBodyElement;
+    }
+
+    function bodyParagraph(innerHTML) {
+        const paragraph = document.createElement("p");
+        paragraph.classList.add("main");
+        paragraph.innerHTML = innerHTML;
+
+        return paragraph;
+    }
+
+    return {
+        mainBody,
+        bodyParagraph,
+    }
+})();
+
+function main() {
+    const main = document.createElement("main");
+    const mainBody = MainPage.mainBody();
+
+    const caption = "<strong>Not all water is created the same!</strong>";
+
+    const captionParagraph = MainPage.bodyParagraph(caption);
+
+    const description = "Don't believe us? Visit Raindrop Cafe and we'll prove it to to you! A sip of water here to introduce yourself to the glorious world of water tasting.";
+
+    const descriptionParagraph = MainPage.bodyParagraph(description);
+
+    mainBody.appendChild(captionParagraph);
+    mainBody.appendChild(descriptionParagraph);
+
+    main.appendChild(mainBody);
+    return main;
+}
+
 //TODO: Credit the background image
 
 const content = document.querySelector("#content");
 content.appendChild(header());
+content.appendChild(main());
